58b411245c9406822c7a90be49e9610379c6ff7b
zinou <zinedine1406@gmail.com>
2025-10-29 14:45:20 +0100

    Authcontext + register page integration

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import { BASE_URL } from "../constants/baseUrl";
+import { useAuth } from "../context/Auth/AuthContext";
 
 const RegisterPage = () => {
   const [error, setError] = useState("");
@@ -10,13 +11,19 @@ const RegisterPage = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
+  const { login } = useAuth();
+
   const onSubmit = async () => {
     const firstName = firstNameRef.current?.value;
     const lastName = lastNameRef.current?.value;
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
 
-    console.log(name, email, password);
+    if (!firstName || !lastName || !email || !password) {
+      return; 
+    }
+
+    
 
     const response = await fetch(`${BASE_URL}/user/register`, {
       method: "POST",
@@ -37,8 +44,15 @@ const RegisterPage = () => {
       return;
     }
 
-    const data = await response.json();
-    console.log(data);
+    const token = await response.json();
+    if (!token) {
+      setError(" incorect token");
+      return;
+    }
+
+    login(email, token);
+
+    console.log(token);
   };
 
   return (
