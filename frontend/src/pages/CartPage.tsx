b43a4851379d58ba5826f5a5fe0c4791b1e3c4a3
zinou <zinedine1406@gmail.com>
2025-11-29 15:29:09 +0100

    cart page layout

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,46 +1,51 @@
 import { Box, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+
 import { useCart } from "../context/Cart/CartContext";
 
 const CartPage = () => {
-  const { token } = useAuth();
-  const {cartItems, totalAmount} = useCart()
-  const [error, setError] = useState('');
-
-  /*
-  useEffect(() => {
- 
-    if(!token){
-        return
-    }
-    const fetchCart = async () => {
-      const response = await fetch(`${BASE_URL}/cart`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        setError("Failed to fetch userCart. please try again ");
-      }
-
-      const data =  await response.json( )
-      setCart(data)
-    };
-
-    fetchCart();
-  },[token]);
-  */
+  const { cartItems, totalAmount } = useCart();
 
   return (
-    <Container sx={{ mt: 2 }}>
+    <Container fixed sx={{ mt: 2 }}>
       <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) =>(
-        <Box>{item.title}</Box>
-      ))}
+      <Box display="flex" flexDirection="column" gap={4}>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              border: 1,
+              borderColor: "#f2f2f2",
+              borderRadius: 5,
+              padding: 1,
+            }}
+          >
+            <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+              <img src={item.image} width={50} />
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} x {item.unitPrice} USD
+                </Typography>
+                <Button>Remove item</Button>
+              </Box>
+            </Box>
+
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>+</Button>
+              <Button>-</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant='h4'>Total Amount : {totalAmount.toFixed(2)} USD</Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
